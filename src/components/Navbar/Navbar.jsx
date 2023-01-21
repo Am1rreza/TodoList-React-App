fa0141ed6f9427f91f6206892c7b673174f6f88e
Amirreza <amirreza.m13825@gmail.com>
2023-01-21 14:44:03 +0330

    Navbar refactored

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,14 @@ import styles from "./navbar.module.css";
 const Navbar = ({ todos }) => {
   const unCompletedTodos = todos.filter((todo) => !todo.isCompleted);
 
+  // conditional rendering
+  if (unCompletedTodos.length === 0)
+    return <h2 style={{ marginBottom: "20px" }}>Set Your Today Todos !</h2>;
+
   return (
     <nav className={styles.nav}>
-      {unCompletedTodos.length === 0 ? (
-        <>
-          <h2>Set Your Today Todos !</h2>
-        </>
-      ) : (
-        <>
-          <span>{unCompletedTodos.length}</span>
-          <h2>Todos Uncompleted</h2>
-        </>
-      )}
+      <span>{unCompletedTodos.length}</span>
+      <h2>Todos Uncompleted</h2>
     </nav>
   );
 };
